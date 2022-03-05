c6995524944c2eab3b8c1730cd53d13f9e0484ac
Larry Leathers <lleathers@naehe.com>
2022-03-05 18:02:37 -0500

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'shaofoodie',
+    appId: '1:368697909663:web:c39244aada029a11201daa',
+    storageBucket: 'shaofoodie.appspot.com',
+    apiKey: 'AIzaSyDZZ_h4orrh9Eqbxepfv77P4msFAfsr0xY',
+    authDomain: 'shaofoodie.firebaseapp.com',
+    messagingSenderId: '368697909663',
+    measurementId: 'G-BE3D4WXH72',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'shaofoodie',
+    appId: '1:368697909663:web:c39244aada029a11201daa',
+    storageBucket: 'shaofoodie.appspot.com',
+    apiKey: 'AIzaSyDZZ_h4orrh9Eqbxepfv77P4msFAfsr0xY',
+    authDomain: 'shaofoodie.firebaseapp.com',
+    messagingSenderId: '368697909663',
+    measurementId: 'G-BE3D4WXH72',
+  },
   production: false
 };
 
